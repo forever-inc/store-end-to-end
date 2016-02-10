4ed65958761fd4a57b8ef5e2d8ec944330f7cd12
Corey Stout <cstout@forever.com>
2016-02-10 11:43:43 -0500

    Fix spec scenario name

--- spec/features/pricing_spec.rb
+++ spec/features/pricing_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Pricing Page:', type: :feature, js: true  do
-  scenario "Empty cart renders successfully" do
+  scenario "Pricing page renders successfully." do
     visit '/'
     expect(page).to have_text('Pricing')
   end
